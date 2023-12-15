abc442312f8574c0985f747be21868a02e46e789
Saqlain Ahsan <saqlain.ahsan@appinventiv.com>
2023-12-15 19:15:06 +0530

    Removed replace_element_in_iterable

--- components/utils/extensions.py
+++ components/utils/extensions.py
@@ -1,25 +1,6 @@
 import json
-import string
 import random
-
-from typing import Callable, TypeVar
-
-T = TypeVar("T")
-
-
-def replace_element_in_iterable(
-    datalist: list[T],
-    element: T,
-    compare: Callable[[T, T], bool] = lambda x, y: x == y,
-) -> bool:
-    try:
-        matches = [datalist.index(x) for x in datalist if compare(x, element)]
-        for match in matches:
-            datalist.pop(match)
-    except KeyError:
-        pass
-
-    datalist.append(element)
+import string
 
 
 def hash_gen(size=6, chars=string.ascii_lowercase):
