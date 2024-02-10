4b8baeb9cc194c1f8b1391f8c01926189c62c2f6
Saqlain Ahsan <saqlain.ahsan@appinventiv.com>
2024-02-10 21:25:56 +0530

    Random Number Generator: Encrypter

--- components/lib/encrypter/aes_encrypter.py
+++ components/lib/encrypter/aes_encrypter.py
@@ -1,10 +1,6 @@
-import hashlib
-
 from Crypto.Cipher import AES
 from Crypto.Hash import SHA256
 from Crypto.Protocol.KDF import PBKDF2
-from Crypto.Random import get_random_bytes
-from Crypto.Random import new as random
 
 from .encrypter import Encrypter
 
@@ -14,34 +10,29 @@ class AesEncrypter(Encrypter):
 
     def __init__(self, secret: str) -> None:
         super().__init__(secret)
-        self.key = self._hash_without_salt(secret)
 
     def _encrypt(self, data: str) -> str:
         raw = self._pad(data)
-        iv = random().read(AES.block_size)
-        cipher = AES.new(self.key, AES.MODE_CBC, iv)
+        iv = self._generate_salt()
+        cipher = AES.new(self._key, AES.MODE_CBC, iv)
         enc = cipher.encrypt(raw.encode(self.encoding))
-        return self._encode_bytes(iv + enc)
+        return self._encode_random_bytes(iv + enc)
 
     def _decrypt(self, data: str) -> str:
-        enc = self._decode_bytes(data)
+        enc = self._decode_random_bytes(data)
         iv = enc[: AES.block_size]
-        cipher = AES.new(self.key, AES.MODE_CBC, iv)
+        cipher = AES.new(self._key, AES.MODE_CBC, iv)
         dnc = cipher.decrypt(enc[AES.block_size :])
         return self._unpad(dnc).decode(self.encoding)
 
     def _hash_with_salt(self, data: str, salt: bytes) -> bytes:
-        return PBKDF2(data, salt, 32, hmac_hash_module=SHA256)
-
-    def _hash_without_salt(self, data: str) -> bytes:
-        return hashlib.sha256(data.encode()).digest()
-
-    def _hash(self, data: str) -> bytes:
-        return self._hash_with_salt(data, self.key)
-
-    @classmethod
-    def _generate_salt(cls) -> bytes:
-        return get_random_bytes(cls.block_size)
+        return PBKDF2(
+            data,
+            salt,
+            self.keysize,
+            self.iterations,
+            hmac_hash_module=SHA256,
+        )
 
     def _pad(self, raw: str) -> str:
         s = raw
