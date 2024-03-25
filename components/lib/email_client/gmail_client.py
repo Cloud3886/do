a497db706d56649ae0f27e6f48e8d8eca151355b
Saqlain Ahsan <saqlain.ahsan@appinventiv.com>
2024-03-25 18:08:09 +0530

    _

--- components/lib/email_client/gmail_client.py
+++ components/lib/email_client/gmail_client.py
@@ -11,7 +11,7 @@ from google.auth.external_account_authorized_user import (
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials as OAuthCredentials
 from google_auth_oauthlib.flow import InstalledAppFlow
-from googleapiclient.discovery import Resource, build
+from googleapiclient.discovery import build
 
 from .email_client import EmailClient
 
